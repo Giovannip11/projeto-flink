75a5e246e4984a2a4b4bed31ff0ec01bac3e1f4d
Giovannip11 <giggiopinto@gmail.com>
2026-05-10 10:24:17 -0300

    Mudanças no docker e mensagem de DDoS

--- flink_job.py
+++ flink_job.py
@@ -49,13 +49,27 @@ t_env.execute_sql(source_ddl)
 print("Submetendo Job de análise para cluster flink...")
 
 
+# Janelas de tempos flink
 query = """
     SELECT
-        Label,
-        COUNT(*) as total_flows,
-        AVG(`Flow Duration`) as avg_duration
-    FROM network_source
-    GROUP BY Label
+        window_start,
+        window_end,
+        `Source IP`,
+        COUNT(*) as total_requisicoes,
+        -- Se passar de 20 requisições em 3 segundos, alerta imediato!
+        -- Ajustamos o limite proporcionalmente ao tempo menor da janela.
+        CASE
+            WHEN COUNT(*) > 20 THEN ':( ALERTA: DDoS Detectado!'
+            ELSE 'Tráfego Normal'
+        END as status_seguranca
+    FROM TABLE(
+        -- HOP(tabela, coluna_tempo, tamanho_do_salto, tamanho_da_janela)
+        -- Aqui, a janela analisa os últimos 3 segundos, mas se atualiza a cada 1 segundo!
+        HOP(TABLE network_source, DESCRIPTOR(ts), INTERVAL '1' SECOND, INTERVAL '3' SECONDS)
+    )
+    GROUP BY window_start, window_end, `Source IP`
+    -- Filtramos para mostrar no terminal apenas o tráfego que de fato é suspeito ou volumoso
+    HAVING COUNT(*) > 10
 """
 
 result_table = t_env.sql_query(query)
